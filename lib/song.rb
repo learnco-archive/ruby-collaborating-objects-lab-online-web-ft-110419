62863cd4414441216b7cdad6011418140d7977b7
Jess F <robotspacefish@gmail.com>
2019-11-19 15:43:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,6 @@ class Song
   end
 
   def artist_name=(name)
-    # artist = self.all.detect { |song| song.artist.name == name || Artist.new(name) }
+    artist = self.all.detect { |song| song.artist.name == name || Artist.new(name) }
   end
 end
