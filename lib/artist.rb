8a8448ad3944e36ef813b80810d3a74765109032
Jess F <robotspacefish@gmail.com>
2019-11-19 03:44:31 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select { |s| s.artist == self }
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def self.find_or_create_by_name(filename)
+    artist_name, song_name = filename.split(" - ")
+    song = Song.new(song_name)
+    # TODO
+  end
+end
+#
+# t = Artist.new("T")
+# v = Artist.new("V")
+# puts Artist.all
